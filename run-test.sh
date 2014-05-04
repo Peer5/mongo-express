013620a51cbe827a2a33154f884d67be173d4f13
Yehonatan Daniv <maggotfish@gmail.com>
2014-05-04 11:40:39 +0300

    Cleanup run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-#NOW=$(date +"%F")
 forever stop app.js
-#forever start -l /opt/logs/$NOW.log --append server_start.js "--expose-gc --nouse-idle-notification"
 forever start --append app.js
